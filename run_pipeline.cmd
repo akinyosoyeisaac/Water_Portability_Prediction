ed1501db1890f1a89292d2ef06fccf6f3ddab690
akinyosoyeisaac <akinyosoyeisaac@gmail.com>
2022-07-24 07:41:05 +0100

    adding the jpg of the metric

---
+++ run_pipeline.cmd
@@ -0,0 +1,4 @@
+python scr/feature_engineering.py --path=params.yaml
+python scr/train_test_split.py --path=params.yaml
+python scr/training.py --path=params.yaml
+python scr/evalution.py --path=params.yaml
